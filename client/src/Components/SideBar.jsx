954a6f70b589a2155fe1374f8000a846b142cc0c
Tharun Kumar S <tharunsaran3662@gmail.com>
2025-05-05 09:10:04 +0530

    Add files via upload

---
+++ client/src/Components/SideBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../Styles/SideBar.css";
+import { FiHome, FiSettings, FiFileText } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+const SideBar = () => {
+  const navigate = useNavigate(); // Get the navigation function
+
+  return (
+    <div className="SideBar">
+      <div className="bar-head">
+        <h4>ERP SYSTEM</h4>
+      </div>
+      <div className="bar-items">
+        <div className="item" onClick={() => navigate("/dashboard")}>
+          <FiHome className="icon" />
+          <span>Dashboard</span>
+        </div>
+
+        <div className="item" onClick={() => navigate("/billing")}>
+          <FiFileText className="icon" />
+          <span>Billing</span>
+        </div>
+
+        <div className="item" onClick={() => navigate("/configuration")}>
+          <FiSettings className="icon" />
+          <span>Configuration</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
